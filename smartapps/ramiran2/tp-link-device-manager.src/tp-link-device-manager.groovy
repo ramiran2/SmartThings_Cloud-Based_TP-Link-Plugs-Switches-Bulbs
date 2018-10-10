45c5e3b13318216bd951d78a8dc62bbf3b9c8ad8
xKillerMaverick <ramiran4@gmail.com>
2018-10-10 18:32:19 -0400

    Update 3.4.0

--- smartapps/ramiran2/tp-link-device-manager.src/tp-link-device-manager.groovy
+++ smartapps/ramiran2/tp-link-device-manager.src/tp-link-device-manager.groovy
@@ -155,7 +155,7 @@ def authPage() {
 			)
 		}
 		section("Page Selector:") {
-			if (userSelectedOptionTwo != "Activate Account" || userSelectedOptionTwo != "Update Account") {
+			if (!userSelectedOptionTwo) {
 				paragraph pageSelectorNullText(), image: getAppImg("error.png")
 			}
 			if (userSelectedOptionTwo =~ "Activate Account") {
@@ -205,7 +205,7 @@ def mainPage() {
 			)
 		}
 		section("Page Selector:") {
-			if (userSelectedOptionOne != "Initial Install" || userSelectedOptionOne != "Add/Remove Devices" || userSelectedOptionOne != "Update/Remove Token") {
+			if (!userSelectedOptionOne) {
 				paragraph pageSelectorNullText(), image: getAppImg("error.png")
 			}
 			if (userSelectedOptionOne =~ "Initial Install") {
