d97d0c083eafb391debd12bba2ce6ff62e51113a
xKillerMaverick <ramiran4@gmail.com>
2018-10-17 21:24:17 -0400

    Update 3.9.0

--- smartapps/ramiran2/tp-link-smartthings-manager.src/tp-link-smartthings-manager.groovy
+++ smartapps/ramiran2/tp-link-smartthings-manager.src/tp-link-smartthings-manager.groovy
@@ -1184,7 +1184,7 @@ def getWikiPageUrl()	{ return "https://github.com/${gitRepo()}/wiki" }
 def getIssuePageUrl()	{ return "https://github.com/${gitRepo()}/issues" }
 def appLabel()	{ return "TP-Link SmartThings Manager" }
 def appNamespace()	{ return (userSelectedNamespace) ? "ramiran2" : "davegut" }
-def gitRepo()		{ return (userSelectedNamespace) ? "${appNamespace()}/SmartThings_Cloud-Based_TP-Link-Plugs-Switches-Bulbs" : "${appNamespace()}/TP-Link-SmartThings"}
+def gitRepo()		{ return (userSelectedNamespace) ? "${appNamespace()}/TP-Link-SmartThings" : "${appNamespace()}/SmartThings_Cloud-Based_TP-Link-Plugs-Switches-Bulbs" }
 def gitPath()		{ return "${gitRepo()}/${gitBranch()}"}
 def betaMarker()	{ return false }
 def sendingCommandSuccess()	{ return "Command Sent to SmartThings Application" }
